e7fed69466421168f4ba1e6ffc1862e0ce313e8c
Rob Selby <robjselby@gmail.com>
2017-06-12 14:08:43 +0100

    8

--- AWS/variables.tf
+++ AWS/variables.tf
@@ -26,3 +26,11 @@ variable "ami" {
     us-west-1-centos7     = "ami-af4333cf"
   }
 }
+
+variable "key_name" {
+  description = "SSH key name in your AWS account for AWS instances."
+}
+
+variable "key_path" {
+  description = "Path to the private key specified by key_name."
+}
