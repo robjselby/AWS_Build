19dcba462b55fe72e635173289f698a992127d5b
Rob Selby <robjselby@gmail.com>
2017-06-12 14:03:38 +0100

    6

--- AWS/build.tf
+++ AWS/build.tf
@@ -2,4 +2,9 @@ resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 
+  connection {
+        user = "ubuntu"
+        private_key = "${file("${var.key_path}")}"
+}
+
 }
