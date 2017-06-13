450bc43bf7ed3caccd9f4ffdcfa8ccc53bf93d2e
Rob Selby <robjselby@gmail.com>
2017-06-13 14:18:34 +0100

    VPC Added

--- AWS/build.tf
+++ AWS/build.tf
@@ -10,8 +10,28 @@ resource "aws_instance" "mybuild" {
   }
 }
 
+resource "aws_vpc" "TF_VPC" {
+  cidr_block = "10.2.0.0/16"
+
+  tags {
+    Name = "My TF VPC"
+  }
+}
+
+resource "aws_subnet" "front_end" {
+  vpc_id     = "${aws_vpc.TF_VPC.id}"
+  cidr_block = "10.1.1.0/24"
+
+  tags {
+    Name = "Main"
+  }
+}
+
+
+
 resource "aws_security_group" "mysg" {
     name = "mybuild_${var.platform}"
+    vpc_id     = "${aws_vpc.TF_VPC.id}"
     description = "Internal traffic + maintenance."
 
     // These are for internal traffic
@@ -34,7 +54,7 @@ resource "aws_security_group" "mysg" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["188.65.56.229/32"]
     }
 
     // This is for outbound internet access
