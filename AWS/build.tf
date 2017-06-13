80c3864157117dc3708439e74b8f0244f6d64066
Rob Selby <robjselby@gmail.com>
2017-06-13 14:57:37 +0100

    Updated sec group id again

--- AWS/build.tf
+++ AWS/build.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "mybuild" {
   ami           = "${lookup(var.ami, "${var.region}-${var.platform}")}"
   instance_type = "t2.micro"
   key_name = "${var.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.mysg.name}"]
+  vpc_security_group_ids = ["${aws_security_group.mysg.id}"]
 
   connection {
         user = "${lookup(var.user, var.platform)}"
